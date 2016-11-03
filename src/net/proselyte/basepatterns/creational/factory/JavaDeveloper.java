cf678454c2fd909314f42b472ee02f1988e0bf3d
stinkymark1 <stinkymark1@gmail.com>
2016-11-03 19:30:28 +0200

    Temp

--- src/net/proselyte/basepatterns/creational/factory/JavaDeveloper.java
+++ src/net/proselyte/basepatterns/creational/factory/JavaDeveloper.java
@@ -1,6 +1,6 @@
 package net.proselyte.basepatterns.creational.factory;
 
-public class JavaDeveloper implements Developer{
+public class JavaDeveloper implements Developer {
     @Override
     public void writeCode() {
         System.out.println("Java developer writes Java code...");
--- src/net/proselyte/basepatterns/creational/factory/Program.java
+++ src/net/proselyte/basepatterns/creational/factory/Program.java
@@ -2,22 +2,10 @@ package net.proselyte.basepatterns.creational.factory;
 
 public class Program {
     public static void main(String[] args) {
-        DeveloperFactory developerFactory = createDeveloperBySpecialty("java");
+        DeveloperFactory developerFactory = createDeveloperBySpecialty("php");
         Developer developer = developerFactory.createDeveloper();
 
         developer.writeCode();
     }
 
-    static DeveloperFactory createDeveloperBySpecialty(String specialty){
-        if(specialty.equalsIgnoreCase("java")){
-            return new JavaDeveloperFactory();
-        }else  if(specialty.equalsIgnoreCase("c++")){
-            return new CppDeveloperFactory();
-        }else if(specialty.equalsIgnoreCase("php")){
-            return new PhpDeveloperFactory();
-        }
-        else {
-            throw new RuntimeException(specialty + " is unknown specialty.");
-        }
-    }
 }
