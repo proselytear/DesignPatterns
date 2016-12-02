085b44a248d053738d73598d7bc459f3bd7b3d87
Eugene Suleimanov <proselytear@yahoo.com>
2016-12-03 01:51:59 +0200

    Update Program.java
    Some style changes.

--- src/net/proselyte/basepatterns/creational/factory/Program.java
+++ src/net/proselyte/basepatterns/creational/factory/Program.java
@@ -2,7 +2,7 @@ package net.proselyte.basepatterns.creational.factory;
 
 public class Program {
     public static void main(String[] args) {
-        DeveloperFactory developerFactory = createDeveloperBySpecialty("php");
+        DeveloperFactory developerFactory = new JavaDeveloperFactory();
         Developer developer = developerFactory.createDeveloper();
 
         developer.writeCode();
